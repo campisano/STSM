1858523752e282cd0ed426de822693bb88ec1672
Riccardo Campisano <riccardo.campisano@gmail.com>
2016-03-20 14:59:08 -0300

    run.sh and display.r refactored

--- R/display.r
+++ R/display.r
@@ -1,9 +1,14 @@
+# configure detailed error outputs
+options(warn=2, keep.source=TRUE);
+
 # loading dependences
 dep = "jsonlite"; if (!require(dep, character.only = TRUE, quietly = TRUE)) {
     install.packages(dep); library(dep, character.only = TRUE);
 }; rm(dep);
 
-
+dep = "TSMining"; if (!require(dep, character.only = TRUE, quietly = TRUE)) {
+    install.packages(dep); library(dep, character.only = TRUE);
+}; rm(dep);
 
 source(file.path("R", "motif_lib.R"));
 load_lib();
@@ -11,98 +16,105 @@ load_lib();
 
 
 args = commandArgs(TRUE);
+print("Arguments:");
+print(args);
+
+
+
 csv_dataset = args[1];
 input_file_json = args[2];
+base_json_path_dir = dirname(input_file_json);
+base_path_dir = dirname(base_json_path_dir);
+base_filename = sub(file.path(base_json_path_dir, ""), "", input_file_json);
+base_filename =  sub("[.][^.]*$", "", base_filename, perl=TRUE);
 
-base_path_dir = dirname(input_file_json);
-base_path_file =  sub("[.][^.]*$", "", input_file_json, perl=TRUE);
+
 
 json_data = fromJSON(file(input_file_json));
 
-
-
-# for(i in 1:nrow(json_data))
-# {
-#     sequence_data_length = json_data[i,]
-# }
-
-
-
-# WARNING! getting only the last sequences, of the bigger length!
-for(len in c(0, 1, 2))
+if(is.null(nrow(json_data)))
 {
-    sequence_data_length = json_data[nrow(json_data) - len,];
-    sequence_length = sequence_data_length$first;
-
-    if(length(sequence_data_length$second) < 1)
+    print("Empty json data");
+} else {
+    # WARNING! getting only the 3 last sequences, of the bigger length!
+    for(len in c(0, 1, 2))
     {
-        print(sequence_data_length);
-        next;
+        sequence_data_length = json_data[nrow(json_data) - len,];
+        sequence_length = sequence_data_length$first;
+
+        if(length(sequence_data_length$second) < 1)
+        {
+            print("Empty sequence data data", len , ":", sequence_data_length);
+            next;
+        }
+
+        sequence_data = sequence_data_length$second[[1]];  # c++ pair
+
+        my_stmotifs = list();
+
+        for(j in 1:nrow(sequence_data))
+        {
+            sequence_data_item = sequence_data[j,];
+            sequence = sequence_data_item$first;
+            sequence = gsub("[()<>]", "", sequence);
+            support = sequence_data_item$second[1,]$first;
+            match_data = sequence_data_item$second[1,]$second[[1]]; # c++ pair
+
+            my_item = list();
+            #my_item[['saxcod']] = data.frame(
+            #    t(strsplit(sequence, split="")), check.names = FALSE);
+            my_item[['saxcod']] = data.frame(
+                t(data.frame(strsplit(sequence, split=""), row.names = NULL)),
+                row.names = NULL);
+            my_item[['isaxcod']] = sequence;
+            my_item[['recmatrix']] = '';
+            my_item[['vecst']] = data.frame(
+                's'=match_data$second, 't'=match_data$first);
+
+            my_stmotifs[[sequence]] = my_item;
+        }
+
+        # cleanup
+        rm(
+            my_item, match_data, support, sequence, sequence_data_item,
+            sequence_data, j, sequence_length, sequence_data_length
+        );
+
+        filename_rdata = file.path(
+            base_path_dir, "rdata",
+            paste(paste(base_filename, len, sep="_-"), "rdata", sep=".")
+        );
+
+        # save a fast readable Rdata file
+        save(my_stmotifs, file=filename_rdata);
+        load(filename_rdata);
+
+
+
+
+        #setwd("/home/t1t0/Desktop/mestrado/GSP")
+        #load("20_10_4_5_C.RData") # candidates
+        #load("t100.RData") # dataset 40x100
+
+        dataset = read.table(
+            file = csv_dataset,
+            header = TRUE,
+            fill = TRUE,
+            as.is = TRUE,
+            stringsAsFactors = FALSE,
+            sep = " ",
+            quote = "");
+        #stmotifs = STSIdentifySTMotifs(candidates, 5, 5)
+        #sttightmotifs = STSIdentifyTightSTMotifs(stmotifs, candidates$rectangles)
+        #ranksttightmotifs = STSRankTightSTMotifs(sttightmotifs)
+
+        filename_png = file.path(
+            base_path_dir, "png",
+            paste(paste(base_filename, len, sep="_-"), "png", sep=".")
+        );
+
+        png(filename_png, bg="transparent");
+        ds = PlotSTMotifs(my_stmotifs);
+        dev.off();
     }
-
-    sequence_data = sequence_data_length$second[[1]];  # c++ pair
-
-    my_stmotifs = list();
-
-    for(j in 1:nrow(sequence_data))
-    {
-        sequence_data_item = sequence_data[j,];
-        sequence = sequence_data_item$first;
-        sequence = gsub("[()<>]", "", sequence);
-        support = sequence_data_item$second[1,]$first;
-        match_data = sequence_data_item$second[1,]$second[[1]]; # c++ pair
-
-        my_item = list();
-        #my_item[['saxcod']] = data.frame(
-        #    t(strsplit(sequence, split="")), check.names = FALSE);
-        my_item[['saxcod']] = data.frame(
-            t(data.frame(strsplit(sequence, split=""), row.names = NULL)),
-            row.names = NULL);
-        my_item[['isaxcod']] = sequence;
-        my_item[['recmatrix']] = '';
-        my_item[['vecst']] = data.frame(
-            's'=match_data$second, 't'=match_data$first);
-
-        my_stmotifs[[sequence]] = my_item;
-    }
-
-    # cleanup
-    rm(
-        my_item, match_data, support, sequence, sequence_data_item,
-        sequence_data, j, sequence_length, sequence_data_length
-    );
-
-    filename_rdata = paste(
-        paste(base_path_file, len, sep="_-"), "RData", sep=".");
-    filename_png = paste(
-        paste(base_path_file, len, sep="_-"), "png", sep=".");
-
-
-
-    # save a fast readable Rdata file
-    save(my_stmotifs, file=filename_rdata);
-    load(filename_rdata);
-
-
-
-
-    #setwd("/home/t1t0/Desktop/mestrado/GSP")
-    #load("20_10_4_5_C.RData") # candidates
-    #load("t100.RData") # dataset 40x100
-
-    dataset = read.table(
-        file = csv_dataset,
-        header = TRUE,
-        fill = TRUE,
-        as.is = TRUE,
-        stringsAsFactors = FALSE,
-        sep = " ",
-        quote = "");
-    #stmotifs = STSIdentifySTMotifs(candidates, 5, 5)
-    #sttightmotifs = STSIdentifyTightSTMotifs(stmotifs, candidates$rectangles)
-    #ranksttightmotifs = STSRankTightSTMotifs(sttightmotifs)
-
-    png(filename_png, bg="transparent");
-    ds = PlotSTMotifs(my_stmotifs);
-    dev.off();
 }
--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-MEM_LIMIT=7340032;     # 7GB
+MEM_LIMIT=7340032;      # 7GB
+#MEM_LIMIT=15728640;     # 15GB
 ulimit -v $MEM_LIMIT;
 
-INPUT_FOLDER="data/";
-OUTPUT_FOLDER="results/";
+INPUT_FOLDER="data";
+OUTPUT_FOLDER="results";
 FILE_NAME="100_sax";
 
 ALPHABETS=("5" "10" "15" "20" "25");
 TYPES=("original" "transposed");
-SUPPORTS=("100" "90" "80");
+SUPPORTS=("100" "95" "90" "85" "80");
 GAPS=("0" "1" "2" "3");
 
-PATH=./BUILD/release:$PATH
+PATH=./BUILD/release:$PATH;
 
 
 
@@ -20,23 +21,60 @@ for TYPE in "${TYPES[@]}"
 do
     for ALPHABET in "${ALPHABETS[@]}"
     do
-        INPUT_FILE=$INPUT_FOLDER$FILE_NAME"-"$ALPHABET"_"$TYPE".csv";
+        INPUT_FILE=$INPUT_FOLDER"/"$FILE_NAME"-"$ALPHABET"_"$TYPE".csv";
 
         for SUPPORT in "${SUPPORTS[@]}"
         do
             for GAP in "${GAPS[@]}"
             do
-                echo "--------------------------------------------------"
-                SPEC_OUTPUT_FOLDER=$OUTPUT_FOLDER$ALPHABET"_"$TYPE"/";
-                OUTPUT_FILE=$SPEC_OUTPUT_FOLDER$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP".json";
-                LOG_FILE=$SPEC_OUTPUT_FOLDER$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP".log";
-                LOG_FILE_R=$SPEC_OUTPUT_FOLDER$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP"_R.log";
-                LOG_FILE_GSP=$SPEC_OUTPUT_FOLDER$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP"_GSP.log";
-                mkdir -p $SPEC_OUTPUT_FOLDER;
-                echo " * Running GSP $TYPE $ALPHABET $SUPPORT $GAP [...]";
-                gsp $INPUT_FILE $OUTPUT_FILE $LOG_FILE $SUPPORT $GAP &> $LOG_FILE_GSP;
-                echo " * Plotting data [...]";
-                Rscript R/display.r $INPUT_FILE $OUTPUT_FILE &> $LOG_FILE_R;
+                echo "--------------------------------------------------";
+                SPEC_OUTPUT_FOLDER=$OUTPUT_FOLDER"/"$ALPHABET"_"$TYPE;
+                mkdir -p $SPEC_OUTPUT_FOLDER"/json/";
+                mkdir -p $SPEC_OUTPUT_FOLDER"/log/";
+                mkdir -p $SPEC_OUTPUT_FOLDER"/png/";
+                OUTPUT_FILE=$SPEC_OUTPUT_FOLDER"/json/"$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP".json";
+                LOG_FILE=$SPEC_OUTPUT_FOLDER"/log/"$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP".log";
+                PNG_FILE=$SPEC_OUTPUT_FOLDER"/png/"$FILE_NAME"-"$ALPHABET"_"$TYPE"_s"$SUPPORT"_g"$GAP"_-0.png";
+
+                # Produce GSP results only if was not already done
+                if test ! -f $OUTPUT_FILE.gz
+                then
+                    if test ! -f $OUTPUT_FILE
+                    then
+                        echo " * Running GSP $TYPE $ALPHABET $SUPPORT $GAP [...]";
+                        gsp $INPUT_FILE $OUTPUT_FILE $LOG_FILE $SUPPORT $GAP;
+                    fi;
+                fi;
+
+                # Produce PNG image oly if was not already done
+                if test ! -f $PNG_FILE
+                then
+                    if test -f $OUTPUT_FILE.gz
+                    then
+                        echo " * Decompressing previous output file [...]";
+                        gzip -d ${OUTPUT_FILE}.gz;
+                    fi;
+                    if test -f $OUTPUT_FILE
+                    then
+                        echo " * Plotting data $TYPE $ALPHABET $SUPPORT $GAP [...]";
+                        R --vanilla --slave --file=R/display.r --args $INPUT_FILE $OUTPUT_FILE;
+                    fi;
+                fi;
+
+                # Compress outputs
+                if test -f $OUTPUT_FILE
+                then
+                    echo " * Compressing ouput file [...]";
+                    rm -f $OUTPUT_FILE.gz;
+                    gzip --fast $OUTPUT_FILE;
+                fi;
+
+                if test -f $LOG_FILE
+                then
+                    echo " * Compressing log file [...]";
+                    rm -f $LOG_FILE.gz;
+                    gzip --fast $LOG_FILE;
+                fi;
             done;
         done;
     done;
